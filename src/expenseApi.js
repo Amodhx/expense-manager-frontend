cf3dee0e43191d1d5224fa5ddb023aea95e6b04b
Amodh <amxdhnanditha@gmail.com>
2026-09-04 16:20:25 +0530

    creaetd filter get

--- src/expenseApi.js
+++ src/expenseApi.js
@@ -1,12 +1,13 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: import.meta.env.BASE_URL || "http://localhost:8000/api",
+    baseURL: "http://localhost:8000/api",
     headers: { "Content-Type": "application/json" },
 });
 
 export const expenseApi = {
-    getAll: () => client.get("/expenses").then((res) => res.data),
+    getAll: (type) =>
+        client.get("/expenses", { params: type ? { type } : {} }).then((res) => res.data),
     getOne: (id) => client.get(`/expenses/${id}`).then((res) => res.data),
     create: (payload) => client.post("/expenses", payload).then((res) => res.data),
 };
